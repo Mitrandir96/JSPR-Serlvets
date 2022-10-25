7ea7e531e457f5908de2fdce8c5c26f422d5b0b6
Илья Грачев <grachev.ia96@gmail.com>
2022-10-25 21:23:57 +0700

    corrected

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
 
   List<Post> posts = new CopyOnWriteArrayList<>();
